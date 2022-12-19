0de1632f5ce86a7c37d65d3b62089b9f500d05fe
dspaiva123 <danielpaiva1964@gmail.com>
2022-12-19 12:47:28 -0300

    Timeout e testes

--- client-udp-ack.c
+++ client-udp-ack.c
@@ -12,12 +12,17 @@
 
 #define SENDER 1
 #define MAX_MSG 1000
+#define din_timeout 1
+
+//TIMEOUT INICIAL:
+__time_t init_timeout_sec = 2;		/* Seconds.  */
+__suseconds_t init_timeout_usec = 0;	/* Microseconds.  */
+
 struct message_t{
 	char id; //0 ou 1 ou 2 (ack)
 	char message[MAX_MSG];
 	uint8_t checksum;
 };
-
 
 typedef struct message_t tMessage;
 
@@ -42,18 +47,18 @@ uint16_t rfc_checksum(void* addr,size_t count) {
        return ~sum;
 }
 
-tMessage make_pkt(char id, char msg[MAX_MSG])
+tMessage make_pkt(char id, char msg[MAX_MSG], u_int16_t checksum)
 {
 	tMessage pkt;
 	pkt.id = id;
 	strcpy(pkt.message,msg);
-	pkt.checksum = rfc_checksum(msg,sizeof(msg));
+	pkt.checksum = checksum;
 	return pkt;
 }
 
 int isCorrupt(tMessage pkt) //VERIFICA SE O CHECKSUM CALCULADO É DIFERENTE DO DO ENVIADO!
 {
-	return (pkt.checksum != rfc_checksum(pkt.message,sizeof(pkt.message)));
+	return (pkt.checksum != rfc_checksum(&pkt.message,sizeof(pkt.message)));
 }
 
 int isACK(tMessage pkt, int expID) //VERIFICA SE EH O ACK ESPERADO
@@ -63,16 +68,28 @@ int isACK(tMessage pkt, int expID) //VERIFICA SE EH O ACK ESPERADO
 }
 
 
-//Calculo do TimeOut médio (finalizar)
-int num_samples = 1; //num amostras
-struct timeval timeout_avg;
-#define START_TIMEOUT_sec 2
-#define START_TIMEOUT_usec 1;
+/* ########## timeout.h #################*/
 
-void init_timeout(struct timeval *tp)
+struct timeval timeout_avg;//Note that timeout stores de AVERAGE timeout to be set
+int samples = 0;
+long int secSum = 0;
+long int usecSum = 0;
+
+double getTime_ms(struct timeval *tp)
 {
-	tp->tv_sec = START_TIMEOUT_sec;
-	tp->tv_usec = START_TIMEOUT_usec;
+	gettimeofday(tp, 0);
+	return (1000 * (double) tp->tv_sec + (double) tp->tv_usec);
+}
+
+/* Update the average timeout by adding to the current avg
+   a given time difference: timevals of final (tf) and initial
+   (ti) time */
+void ev_timeout(struct timeval *ti, struct timeval *tf){
+    secSum += (tf->tv_sec - ti->tv_sec); //diff
+    usecSum += (tf->tv_usec - ti->tv_usec);
+    samples++;
+    timeout_avg.tv_sec = (uint16_t) (secSum/samples);
+    timeout_avg.tv_usec = (uint16_t) (usecSum/samples);
 }
 
 void set_timeout(int fd, struct timeval timeout)
@@ -82,37 +99,33 @@ void set_timeout(int fd, struct timeval timeout)
 		perror("setsockopt(..., SO_RCVTIMEO ,...");
 }
 
-double getTime_ms()
-{
-	struct timeval tp;
-	gettimeofday(&tp, 0);
-	double seconds = tp.tv_sec;
-	double microseconds = tp.tv_usec;
-	return (1000 * seconds + microseconds);
-}
+// ############### RDT ###############################
 
 char currPkt = 0; //inicializa enviando o pacote 0
 
-void rdt_send(int fd, tMessage request, tMessage response, struct sockaddr_in servaddr)
+void rdt_send(int fd, char data[MAX_MSG], struct sockaddr_in servaddr)
 {
 	int currerrno = errno; //keep track of errno to check if there was a timeout
 	int keepGoing = 1;
 
-	//Parte do timeout movel
-	double t1 = 0;
-	double t2 = 0;
-	struct timeval cTime;
-	//fim da parte do timeout movel
+	//Parte do timeout movel:
+	struct timeval t0, tf;
+	
+	//Calculando o checksum:
+	uint16_t chk = 0; //NAO FUNCIONA O CHECKSUM!!!!!
+	tMessage request = make_pkt(currPkt, data, chk);
 
 	while(keepGoing == 1){
-		t1 = getTime_ms();
+		if(din_timeout) getTime_ms(&t0); //get initial time
+
 		sendto(fd, (tMessage*)&request, sizeof(request), MSG_CONFIRM,
 			(struct sockaddr *) &servaddr, sizeof(servaddr));
 
-		printf("Message Sent\n");
+		printf("sendto: Message Sent\n");
 
-
+		//Check the ack:
 		int res, len;
+		tMessage response; //redeclare the response all times!!
 		len = sizeof(struct sockaddr_in);
 		res = recvfrom(fd, (tMessage*)&response, sizeof(response), MSG_WAITALL,
 				(struct sockaddr *) &servaddr, &len);
@@ -120,17 +133,42 @@ void rdt_send(int fd, tMessage request, tMessage response, struct sockaddr_in se
 		//NÓ de Wait for ACK
 		//verificar o response se foi o ACK certo e o ack do pacote enviado
 
-		if (errno != EAGAIN)
+		if (errno != EAGAIN) //Se nao chegou o pacote, reenviar o pacote!
 		{
-			printf("Acknoledgment: %d\n", response.id);
-			keepGoing = 0;
-			t2 = getTime_ms();
-			double timeElapsed = t2 - t1;
-			printf("Elapsed = %f\n", timeElapsed);
+			//Pacote recebido: Ajustar o timeout medio
+			if(din_timeout)
+			{
+				getTime_ms(&tf); //INDEPENDENTE DE SE FOR O CERTO, o timeout é em cima de um pacote enviar e seu tempo a ser recevido de volta
+				ev_timeout(&t0,&tf);
+				set_timeout(fd, timeout_avg);
+			}
+			
+			printf("Packet Recieved:\n| ID: %d\n| Content: %s\n| Cksum: %d", response.id, response.message, response.checksum);
+			//Pacote recebido: conferir se eh o ack certo!!
+			//Check if it is corrupted:
+			if (isCorrupt(response))
+			{
+				printf("rdt_send: Corrupted Package recieved: Trying again...\n");
+				errno = 0;
+			} 
+			else
+			{
+				//Check the ack recieved:
+				if (isACK(response, currPkt)){
+					currPkt = !currPkt; //Next package will be: 1, if current was 0. 0, if current was 1.
+					keepGoing = 0; //OK!
+					printf("sendto: Message Succesfully Sent :D \n");
+				} 
+				else 
+				{
+					printf("rdt_send: ACK not recieved: Trying again...\n");
+					errno = 0;
+				}
+			}	
 		}
 		else
 		{
-			printf("Timeout! Trying again...\n");
+			printf("recv_from: Timeout! Trying again...\n");
 			errno = 0; //reset errno to try again
 		}
 	}
@@ -189,12 +227,14 @@ int main(int argc, char* argv[])
        		exit(EXIT_FAILURE);
 	}
 
+	//Conffigurando o TIMEOUT
 	struct timeval timeout;
-	timeout.tv_sec = 5; // segundos
-	timeout.tv_usec = 0; // microssegundos
+	timeout.tv_sec = init_timeout_sec; // segundos
+	timeout.tv_usec = init_timeout_usec; // microssegundos
 	// timeout para recebimento
 	if (setsockopt (fd, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(timeout)) < 0)
 		perror("setsockopt(..., SO_RCVTIMEO ,...");
+
 
 	memset(&servaddr, 0, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
--- rfc_checksum.c
+++ rfc_checksum.c
@@ -10,14 +10,22 @@
 #include <inttypes.h>
 #include <sys/time.h>
 
+#define MAX_MSG 1000
+struct message_t{
+	char id; //0 ou 1 ou 2 (ack)
+	char message[MAX_MSG];
+	uint16_t checksum;
+};
+
+typedef struct message_t tMessage;
 
 uint16_t rfc_checksum(void* addr,size_t count) {
-    register long sum = 0;
+    register long long sum = 0;
 
         while( count > 1 )  {
            /*  This is the inner loop */
                sum += * (unsigned short *) addr++;
-               count -= 2;
+               count -= 1;
        }
 
            /*  Add left-over byte, if any */
@@ -31,15 +39,69 @@ uint16_t rfc_checksum(void* addr,size_t count) {
        return ~sum;
 }
 
-int test(int data){
-    return (data != 1);
+/* ########## timeout.h #################*/
+
+struct timeval timeout;//Note that timeout stores de AVERAGE timeout to be set
+int samples = 0;
+long int secSum = 0;
+long int usecSum = 0;
+
+double getTime_ms(struct timeval *tp)
+{
+	gettimeofday(tp, 0);
+	return (1000 * (double) tp->tv_sec + (double) tp->tv_usec);
+}
+
+/* Update the average timeout by adding to the current avg
+   a given time difference: timevals of final (tf) and initial
+   (ti) time */
+void ev_timeout(struct timeval *ti, struct timeval *tf){
+    secSum += (tf->tv_sec - ti->tv_sec); //diff
+    usecSum += (tf->tv_usec - ti->tv_usec);
+    samples++;
+    timeout.tv_sec = (uint16_t) (secSum/samples);
+    timeout.tv_usec = (uint16_t) (usecSum/samples);
+}
+
+/* ########## MAIN PROGRAM #################*/
+u_int16_t check(char data[1000]){
+    return rfc_checksum(data, sizeof(data)*8);
+}
+void test(char data[1000]){
+    //printf("%d + %d", data[0], sizeof(data));
+    printf("\n%d\n", check(data));
+}
+
+int isCorrupt(tMessage pkt) //VERIFICA SE O CHECKSUM CALCULADO É DIFERENTE DO DO ENVIADO!
+{
+	return (pkt.checksum != rfc_checksum(pkt.message,strlen(pkt.message)));
 }
 
 int main(int argc, char const *argv[])
 {
-    char *aa = "1";
-    int data;
-    data = atoi(aa);
-    printf("\n%d\n", test(data));
+    struct timeval t1, t2;
+    double ms1,ms2;
+
+   /*  for(int i = 0; i < 10; i++){
+        ms1 = getTime_ms(&t1);
+        printf("\n%ld e %ld\n", t1.tv_sec, t1.tv_usec);
+        sleep(i);
+        ms2 = getTime_ms(&t2);
+        printf("\n%ld e %ld\n", t2.tv_sec, t2.tv_usec);
+        ev_timeout(&t1, &t2);
+        printf("\nTIMEOUT: %ld e %ld\n", timeout.tv_sec, timeout.tv_usec);
+    } */
+
+    tMessage msg;
+    char pkt = 1;
+    strcpy(msg.message, argv[1]);
+    uint16_t ev = rfc_checksum(argv[1], strlen(argv[1]));
+    printf("\n%s - %d\n", argv[1], ev);
+    msg.checksum = rfc_checksum(msg.message,strlen(msg.message));
+    printf("\n%s - %d\n", msg.message, msg.checksum);
+    printf("\n%d\n", !pkt);
+    //test(msg.message);
+
+    
     return 0;
 }
